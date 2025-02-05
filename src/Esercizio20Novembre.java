2f3757277dd1091d5f9b773320fb6b20d4c16448
elenafrosi <frosi23900@iistorriani.intranet>
2025-02-05 12:55:57 +0100

    vai a quel paese

---
+++ src/Esercizio20Novembre.java
@@ -0,0 +1,9 @@
+
+public class Esercizio20Novembre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona persona1 = new Persona();
+	}
+
+}
---
+++ src/Persona.java
@@ -0,0 +1,17 @@
+
+public class Persona {
+	String nome;
+	String cognome;
+	int eta;
+	String colorePreferito;
+	
+	// TODO Auto-generated constructor stub
+	public Persona(String nom, String cogn, int eta, String colore) {
+		this.nome = nom;
+		this.cognome = cogn;
+		this.eta = eta;
+		this.colorePreferito = colore;
+	}
+
+}
+
